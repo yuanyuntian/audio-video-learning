4ebe0425cd0b1910cc61336c8fc2b58b1355dca3
yuanyuntian <yuanf1990@163.com>
2019-01-24 11:48:37 +0800

    修改工程名 添加SEAugraph

---
+++ SEAUGraph/SEAUGraphTests/SEAUGraphTests.m
@@ -0,0 +1,37 @@
+//
+//  SEAUGraphTests.m
+//  SEAUGraphTests
+//
+//  Created by yuan on 2019/1/24.
+//  Copyright © 2019年 sunEEE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SEAUGraphTests : XCTestCase
+
+@end
+
+@implementation SEAUGraphTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
